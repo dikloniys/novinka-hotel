f4c1cb1d682f78bdfa858c5327ea6f7acd6c0c60
dikloniys <vavan01099400@yandex.ru>
2021-11-16 10:31:22 +0300

    icon_social cursor: pointer

--- local/js/sliderRoom.js
+++ local/js/sliderRoom.js
@@ -1,17 +1,5 @@
-
-$('.slider').slick();
-$('.slider_for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider_bar').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true
-});
+const mainSliderCatalogOptions = {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+}
+var swiper = new Swiper(".js-main-swiper-catalog", mainSliderCatalogOptions);
