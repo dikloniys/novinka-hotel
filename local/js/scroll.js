21c304b71248db1e3243836bd973e756bc017be0
Eugene <rndjester@mail.ru>
2021-11-19 17:08:22 +0300

    Добавлен работающий скролл шапки

--- local/js/scroll.js
+++ local/js/scroll.js
@@ -1,47 +1,48 @@
 $(function() {
-    let header = $('.header');
-
+    let header = $(".js-scroll-check");
+    let firstBlockHeight = $(".js-first-block").height();
+    console.log(firstBlockHeight);
     $(window).scroll(function() {
-    if($(this).scrollTop() > 105) {
-    header.addClass('fixed');
-    } else {
-    header.removeClass('fixed');
-    }
+        if ($(this).scrollTop() > firstBlockHeight) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
     });
 });
-$(function(){
+// $(function(){
 
-    let CurrentScroll = 0;
-    $(window).scroll(function(){
-        let NextScroll = $(this).scrollTop();
-        let hidden = $('.header_nav');
-        let button = $('.js-header_button');
-        if (NextScroll > CurrentScroll){
+//     let CurrentScroll = 0;
+//     $(window).scroll(function(){
+//         let NextScroll = $(this).scrollTop();
+//         let hidden = $(".header_nav");
+//         let button = $(".js-header_button");
+//         if (NextScroll > CurrentScroll){
             
-            if($(this).scrollTop() > 105) {
-                hidden.addClass('fixed_hidden');
-                button.removeClass('button_transparent');
-                button.addClass('button');
+//             if($(this).scrollTop() > 105) {
+//                 hidden.addClass("fixed_hidden");
+//                 button.removeClass("button_transparent");
+//                 button.addClass("button");
 
-                } else {
-                    hidden.removeClass('fixed_hidden');
-                    button.addClass('button_transparent');
-                    button.removeClass('button');
-                }
-        }
-        else {
-            hidden.removeClass('fixed_hidden');
-            if($(this).scrollTop() > 105) {
-                button.removeClass('button_transparent');
-                button.addClass('button');
+//                 } else {
+//                     hidden.removeClass("fixed_hidden");
+//                     button.addClass("button_transparent");
+//                     button.removeClass("button");
+//                 }
+//         }
+//         else {
+//             hidden.removeClass("fixed_hidden");
+//             if($(this).scrollTop() > 105) {
+//                 button.removeClass("button_transparent");
+//                 button.addClass("button");
 
-                }
-                else {
-            button.addClass('button_transparent');
-            button.removeClass('button');
-        }
-        }
+//                 }
+//                 else {
+//             button.addClass("button_transparent");
+//             button.removeClass("button");
+//         }
+//         }
 
-        CurrentScroll = NextScroll;
-    });
-});
+//         CurrentScroll = NextScroll;
+//     });
+// });
